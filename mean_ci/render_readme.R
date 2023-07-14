76680cc59875614fdc533a3e80e35f4065987d06
Matt Cook <mattcook204@gmail.com>
2023-07-14 18:29:16 -0500

    add code in render_reame to delete existing markdown files

--- mean_ci/render_readme.R
+++ mean_ci/render_readme.R
@@ -1,5 +1,7 @@
 library(rmarkdown)
 
+unlink("mean_ci/README*", recursive = TRUE)
+
 render("mean_ci/mean_ci.Rmd", 
        md_document(variant = "markdown_github"), 
        output_file = "README.md")
